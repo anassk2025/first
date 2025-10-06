f0c94f464f8c09c7dd531ce27a0c0cf01760f054
anassk2025 <anasshaik205205@gmail.com>
2025-10-06 13:06:24 +0530

    first checkin

---
+++ DoWhileExample.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+public class DoWhileExample
+{
+	public static void main(String args[])
+	{
+		int i=1;
+		do
+		{
+			System.out.println("number:"+i);
+			i++;
+		}
+		while(i<=65009);
+	}
+}
